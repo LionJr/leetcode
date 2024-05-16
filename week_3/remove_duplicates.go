fa6f162baf13860d45422aac3b30748c06d8d8cc
Jorayev_Arslan <arslan.enigma.man@gmail.com>
2024-05-16 11:32:37 +0500

    leetcode tasks, week 3

---
+++ week_3/remove_duplicates.go
@@ -0,0 +1,42 @@
+package week_3
+
+/*
+You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
+
+We repeatedly make duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+
+
+
+Example 1:
+
+Input: s = "abbaca"
+Output: "ca"
+Explanation:
+For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+
+Example 2:
+
+Input: s = "azxxzy"
+Output: "ay"
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+*/
+
+func removeDuplicates(s string) string {
+	stack := make([]byte, 0)
+
+	for i := range s {
+		if len(stack) > 0 && stack[len(stack)-1] == s[i] {
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, s[i])
+		}
+	}
+
+	return string(stack)
+}
